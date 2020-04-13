3dc7f163675532ff556446f140f64dfbf33fac97
KCL-DreiKlite <player124564615@gmail.com>
2020-04-14 00:45:20 +0800

    Finish building Gaming Area Panel. Also add a picture to visualize Game UI.

--- battleshipGame.java
+++ battleshipGame.java
@@ -11,7 +11,6 @@ import javax.swing.SwingUtilities;
 
 
 class GameUI extends JFrame {
-
     /**
      * The field will split into two parts: visiable part, and excutting part.
      * The actual function will run in excutting part, and update visiable part to sync two parts.
@@ -24,9 +23,17 @@ class GameUI extends JFrame {
     ArrayList<JLabel> enemyFieldVisiablePart = new ArrayList<JLabel>();
     ArrayList<SeaGrid> enemyFieldExcuttingPart = new ArrayList<SeaGrid>();
 
+    //The gaming area. Both player's and enemy's field pane will be placed into this panel.
+    JPanel gamingAreaPanel = new JPanel(null);
+
+    //These labels are created to show which field is whos.
+    JLabel lblPlayerFieldInfo = new JLabel("Player side");
+    JLabel lblEnemyFieldInfo = new JLabel("Enemy side");
+
     //These panel will fill with visiable part which instead of the sea grid.
     JPanel playerFieldPane = new JPanel(null);  //Set layout = null
     JPanel enemyFieldPane = new JPanel(null);
+
 
     //Store the side length of the field.
     //The actual field size will be fieldSideLength ^ 2.
@@ -34,7 +41,14 @@ class GameUI extends JFrame {
 
     //Define each grid's side length.
     final int gridSideLength = 20;
-    
+
+    //Store the standard font params and create font style.
+    final String fontName = "Arial";
+    final int fontStyle = Font.PLAIN;
+    final int fontSize = 16;
+    final Font standardFontStyle = new Font(fontName, fontStyle, fontSize);
+
+
     //Convert specific grid's location to target arraylist index.
     //Notice that field will render x first, then be y.
     private int convertGridLocationToArraylistIndex(int x, int y) {
@@ -46,12 +60,8 @@ class GameUI extends JFrame {
         return new Point(index % fieldSideLength, index / fieldSideLength);
     }
 
-    //Add components
-    private void addComponents() {
-        Container cp = getContentPane();
-        cp.setLayout(null);     //We'll place components by giving location directly.
-
-        
+    //Create gaming area panel, include with both player's and enemy's field pane.
+    private void createGamingAreaPanel(Container container) {
         //Render both player's and enemy's field
         for (int y = 0; y < fieldSideLength; y++) {
             for (int x = 0; x < fieldSideLength; x++) {
@@ -71,22 +81,39 @@ class GameUI extends JFrame {
         for (Iterator<JLabel> iterator = enemyFieldVisiablePart.iterator(); iterator.hasNext(); )
             enemyFieldPane.add(iterator.next());
         
-        //Add both these field into conatiner pane
-        cp.add(playerFieldPane);
-        cp.add(enemyFieldPane);
+        //Add both these field into gaming area panel.
+        gamingAreaPanel.add(playerFieldPane);
+        gamingAreaPanel.add(enemyFieldPane);
 
+        //Make some labels below these field panel to show information.
+        gamingAreaPanel.add(lblPlayerFieldInfo);
+        gamingAreaPanel.add(lblEnemyFieldInfo);
+
+        //Last, add gaming area into cp.
+        container.add(gamingAreaPanel);
     }
 
-    //Setup components' location and size
-    private void setupComponentsLocationAndSize() {
+    //Add components.
+    private void addComponents() {
+        Container cp = getContentPane();
+        cp.setLayout(null);     //We'll place components by giving pixel-location directly.
+
+        //Gaming Area.
+        createGamingAreaPanel(cp);
+    }
+
+    //Setup components' location and size from Gaming Area Panel. Also gamingArea itself.
+    private void setupGamingAreaPanelLocationAndSize() {
+        //The width between two field pane.
+        final int widthBetweenTwoPane = 25;
+
         //Setup field's size and location
         enemyFieldPane.setSize(fieldSideLength*gridSideLength, fieldSideLength*gridSideLength);
         enemyFieldPane.setLocation(0, 0);
-
         playerFieldPane.setSize(fieldSideLength*gridSideLength, fieldSideLength*gridSideLength);
-        playerFieldPane.setLocation(playerFieldPane.getX()+enemyFieldPane.getWidth()+50, 0);
+        playerFieldPane.setLocation(playerFieldPane.getX()+enemyFieldPane.getWidth()+widthBetweenTwoPane, 0);
 
-        //Setup JLabels' location and size.
+        //Setup SeaGrid visiable part's location and size.
         for (int x = 0; x < fieldSideLength; x++) {
             for (int y = 0; y < fieldSideLength; y++) {
                 JLabel playerGrid = playerFieldVisiablePart.get(convertGridLocationToArraylistIndex(x, y));
@@ -102,11 +129,39 @@ class GameUI extends JFrame {
             }
         }
 
+        //Set field information labels' size.
+        final Dimension fieldInfoLabelSize = new Dimension(100, 20);
+        lblPlayerFieldInfo.setSize(fieldInfoLabelSize);
+        lblEnemyFieldInfo.setSize(fieldInfoLabelSize);
+
+        //Set field information label below and middle of field pane.
+        final int spaceBetweenFieldPaneAndInfoPane = 5;
+        Point playerFieldPaneLocation = new Point();    //I define this for more convinent calculate.
+        playerFieldPaneLocation.x = playerFieldPane.getX()+(playerFieldPane.getWidth()-lblPlayerFieldInfo.getWidth())/2;
+        playerFieldPaneLocation.y = playerFieldPane.getY()+playerFieldPane.getHeight()+spaceBetweenFieldPaneAndInfoPane;
+        lblPlayerFieldInfo.setLocation(playerFieldPaneLocation);
+        Point enemyFieldPaneLocation = new Point();
+        enemyFieldPaneLocation.x = enemyFieldPane.getX()+(enemyFieldPane.getWidth()-lblEnemyFieldInfo.getWidth())/2;
+        enemyFieldPaneLocation.y = enemyFieldPane.getY()+enemyFieldPane.getHeight()+spaceBetweenFieldPaneAndInfoPane;
+        lblEnemyFieldInfo.setLocation(enemyFieldPaneLocation);
+
+        //Last, setup gaming areea panel's location and size.
+        gamingAreaPanel.setLocation(0, 0);
+        gamingAreaPanel.setSize(playerFieldPane.getX()+playerFieldPane.getWidth(), lblPlayerFieldInfo.getY()+lblPlayerFieldInfo.getHeight());
+
+
+    }
+
+    
+    //Setup components' location and size
+    private void setupComponentsLocationAndSize() {
+        //Gaming Area.
+        setupGamingAreaPanelLocationAndSize();
         
     }
 
-    //Setup components' details.
-    private void setupComponentsDetails() {
+    //setup components' details from gaming area panel.
+    private void setupGamingAreaPanelDetails() {
         //Setup each grid's background color and border.
         for (Iterator<JLabel> iterator = enemyFieldVisiablePart.iterator(); iterator.hasNext(); ) {
             JLabel tmp = iterator.next();
@@ -123,15 +178,38 @@ class GameUI extends JFrame {
             tmp.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));  //Set color.
         }
 
-        
+        //Set field info label font.
+        lblPlayerFieldInfo.setFont(standardFontStyle);
+        lblEnemyFieldInfo.setFont(standardFontStyle);
+
+        //Let the text be center in field info label.
+        lblPlayerFieldInfo.setHorizontalAlignment(JLabel.CENTER);
+        lblPlayerFieldInfo.setVerticalAlignment(JLabel.CENTER);
+        lblEnemyFieldInfo.setHorizontalAlignment(JLabel.CENTER);
+        lblEnemyFieldInfo.setVerticalAlignment(JLabel.CENTER);
+
+        //Make a border suround field pane.
+        // playerFieldPane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        // enemyFieldPane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+
+
+    }
+
+    //Setup components' details, like border, background, font, etc.
+    private void setupComponentsDetails() {
+        //Gaming Area Panel.
+        setupGamingAreaPanelDetails();
+
         
     }
 
 
-    //Setup frame
+    //Setup Game UI main setting.
     private void setupFrame() {
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //getContentPane().setFont(new Font("Arial", Font.PLAIN, 20));
 
         setPreferredSize(new Dimension(1000, 500));
         pack();
@@ -140,6 +218,8 @@ class GameUI extends JFrame {
     //Constructor
     GameUI() {
         super();
+        
+        
 
         addComponents();
         setupComponentsLocationAndSize();
